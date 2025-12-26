e8ed6a4d03b10fe305d5180c4f605abd79064b7a
Elisabeth Weise <weiseel@yahoo.com>
2025-12-26 13:56:22 +0100

    fortsätter med djupanalysen

--- data/vis8_portfolio.sql
+++ data/vis8_portfolio.sql
@@ -3,18 +3,16 @@ USE AdventureWorks2025;
 SELECT TOP 5 * FROM Production.Product;
 SELECT TOP 5 * FROM Production.ProductCostHistory;      ---- var jag kan hitta kostanden per produkt historiskt
 
---- skillnanden mellan Production.ProductCostHistory och Production.StandardCost:
+--- skillnanden mellan Production.ProductCostHistory.StandardCost och Production.Product.StandardCost:
 --- Production.ProductCostHistory har ett start datum för när kostanden började implementeras och ett 
 --- slutdatum där kostnaden ändrades. Om slutdatum är NULL gäller det fortfarande kostnaden. 
 --- Därför borde de kostander som har ett slutdatum som är NULL vara de samma som finns i Production.StandardCost
---- jag gör därför valtet att bara använda mig av Production.ProductCostHistory då det är en mindre tabell att
---- använda mig av och borde ge samma resultat
-
+--- jag gör därför valvet att bara använda mig av Production.ProductCostHistory
 SELECT 
     ProductID,
     Name,
     StandardCost
-FROM Production.Product;        --- ser att vissa produkter har 0.00 som StandardCost
+FROM Production.Product;        --- vissa produkter har 0.00 som StandardCost
 
 SELECT 
     ProductID,
@@ -41,6 +39,37 @@ JOIN Sales.SalesOrderDetail sod
 WHERE soh.OrderDate >= '2024-05-01'
   AND soh.OrderDate <  '2025-05-01'
 GROUP BY sod.ProductID;
+
+---12 månader fram till 30 april 2025
+--- Vinst per produkt
+
+SELECT TOP 10
+    sod.ProductID,
+    p.Name AS Produktnamn,
+    pc.Name AS Produktkategori,
+    SUM(sod.LineTotal) AS Intäkt,
+    SUM(sod.OrderQty * pch.StandardCost) AS Kostnad,
+    SUM(sod.LineTotal) - SUM(sod.OrderQty * pch.StandardCost) AS Vinst
+FROM Sales.SalesOrderHeader soh
+JOIN Sales.SalesOrderDetail sod
+    ON soh.SalesOrderID = sod.SalesOrderID
+JOIN Production.Product p  
+    ON sod.ProductID = p.ProductID
+JOIN Production.ProductSubcategory psc
+    ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+JOIN Production.ProductCategory pc
+    ON psc.ProductCategoryID = pc.ProductCategoryID
+JOIN Production.ProductCostHistory pch
+    ON pch.ProductID = sod.ProductID
+   AND pch.StartDate <= soh.OrderDate
+   AND (pch.EndDate IS NULL OR pch.EndDate > soh.OrderDate)
+WHERE soh.OrderDate >= '2024-05-01'
+  AND soh.OrderDate <  '2025-05-01'
+  AND pch.StandardCost > 0
+GROUP BY sod.ProductID,
+        p.Name,
+        pc.Name
+ORDER BY Vinst DESC;
 
 --- vinst per produkt, sorterat på vinstmarginal
 SELECT
@@ -75,3 +104,35 @@ ORDER BY Vinstmarginal DESC;
 
 
 ---- skilland mellan tillverkade produkter och återförsäljningsprodukter?
+
+
+SELECT
+    TOP 10
+    sod.ProductID,
+    p.Name AS Produktnamn,
+    pc.Name AS Produktkategori,
+    SUM(sod.LineTotal) AS Intäkt,               --- Försäljning, använder LineTotal som tidigare
+    SUM(sod.OrderQty * pch.StandardCost) AS Kostnad,
+    SUM(sod.LineTotal) - SUM(sod.OrderQty * pch.StandardCost) AS Vinst, --- försäljning minus kostnad
+    (SUM(sod.LineTotal) - SUM(sod.OrderQty * pch.StandardCost))  
+      / NULLIF(SUM(sod.LineTotal), 0) AS Vinstmarginal                  --- vinstmarginal
+FROM Sales.SalesOrderHeader soh
+JOIN Sales.SalesOrderDetail sod
+    ON soh.SalesOrderID = sod.SalesOrderID
+JOIN Production.Product p
+    ON sod.ProductID = p.ProductID
+JOIN Production.ProductSubcategory psc
+    ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+JOIN Production.ProductCategory pc
+    ON psc.ProductCategoryID = pc.ProductCategoryID
+JOIN Production.ProductCostHistory pch
+    ON pch.ProductID = sod.ProductID
+   AND pch.StartDate <= soh.OrderDate
+   AND (pch.EndDate IS NULL OR pch.EndDate > soh.OrderDate)
+WHERE soh.OrderDate >= '2024-05-01'
+  AND soh.OrderDate <  '2025-05-01'
+  AND pch.StandardCost > 0
+GROUP BY
+    sod.ProductID, 
+    p.Name, 
+    pc.Name;
